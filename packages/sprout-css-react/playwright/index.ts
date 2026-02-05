be4515ff2fa330dda6a3150d4cf1a7bdd4f1e6c7
Jean-Michel FRANCOIS <jeanmichel.francois@qlik.com>
2026-02-05 11:09:22 +0100

    fix: remove test-results

--- packages/sprout-css-react/playwright/index.ts
+++ packages/sprout-css-react/playwright/index.ts
@@ -1,4 +1,8 @@
 // Apply theme here, add anything your component needs at runtime here.
-import { applyTheme } from "@qlik/sprout-theme-provider";
 
-applyTheme(document.body);
+document.body.dataset.qlikTheme = "qlik-light";
+document.body.dataset.qlikAppearance = "qlik-light";
+document.body.dataset.qlikDensity = "comfortable";
+document.body.dataset.qlikRoundness = "soft";
+document.body.dataset.qlikSizing = "mid-sized";
+document.body.dataset.qlikTypography = "source-sans";
